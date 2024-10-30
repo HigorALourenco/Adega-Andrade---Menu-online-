40e9f520007bbcbdc1ba8492aa1c5de2c87f8af7
Higor Lourenço <higor.augusto1993@outlook.com>
2024-10-30 11:04:08 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -173,7 +173,7 @@ checkoutBtn.addEventListener('click', function(){
 function checkRestaurantOpen(){
     const data = new Date();
     const hora = data.getHours();
-    return hora >= 18 && hora < 1;
+    return hora >= 9 && hora < 1;
     // true = adega está aberta
 }
 
